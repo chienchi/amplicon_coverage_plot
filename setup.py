f794f5704c631738a639df22f4fad98a94d0402d
Chien-Chi Lo <chien-chi@pn1704856dongle.lanl.gov>
2021-01-26 09:46:53 -0700

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='amplicov',
-    version= '0.1.4',
+    version= '0.2.0',
     author='Chienchi Lo',
     author_email='chienchi@lanl.gov',
     packages=find_packages(),
