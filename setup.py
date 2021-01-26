b2158928467a19094239c3f76cfff82a0904ce1c
Chien-Chi Lo <chien-chi@pn1704856dongle.lanl.gov>
2021-01-25 22:15:46 -0700

    1. fix bug:  unique region parsing from bed/bedpe file. It was missing the first and last region and error when there is fully covered amplicon by another amplicon. 2. expose the min coverage as parameter to let user define the ambigous N and default to 10 3. fix bug: the interatcive plot with new added trace to show number of ambiguous.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='amplicov',
-    version= '0.1.3',
+    version= '0.1.4',
     author='Chienchi Lo',
     author_email='chienchi@lanl.gov',
     packages=find_packages(),
