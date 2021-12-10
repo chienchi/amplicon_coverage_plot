3d5f9b9d38c9af38cf4b57d8e1dd6107f0021523
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2021-12-09 21:25:43 -0700

    bump to 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='amplicov',
-    version= '0.3.1',
+    version= '0.3.2',
     author='Chienchi Lo',
     author_email='chienchi@lanl.gov',
     packages=find_packages(),
